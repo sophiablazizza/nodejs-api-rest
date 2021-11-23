b31b8aa0b74524015471e43235655ad3a81150b6
Sophia Blazizza <sblazizza@gmail.com>
2021-11-23 17:34:18 -0300

    feat: rota para criar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
     app.get ('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos e esta realizando um GET'))
+
+    app.post ('/atendimentos', (req,res) => res.send ('Você está na rota de arendimentos e esta realizando um POST'))
 }
