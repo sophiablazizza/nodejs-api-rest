c56bbeb526503c66d42a296b679411474bb0cb0b
Sophia Blazizza <sblazizza@gmail.com>
2021-11-24 10:53:15 -0300

    feat: adicionando tratamento de erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,17 +6,44 @@ class Atendimento {
     adiciona (atendimento, res) {
         const dataCriacao = moment(). format ('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento, data, 'DD/MM/YYYY'). format ('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
         
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataEhValida = moment (data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atentimento.cliente.length >= 5
 
-        conexao.query(sql, atendimentoDatado, (erro) => {
-            if (erro) {
-                res.status(400).json('erro')
-            } else {
-                res.status(201).json('resultados')
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro) => {
+                if (erro) {
+                    res.status(400).json('erro')
+                } else {
+                    res.status(201).json('resultados')
+                }
+            })
+
+        }
+
+        
     }
 }
 
