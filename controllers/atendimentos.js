19ee5496a838845caabbb416b1aeaa67c4860a3c
Sophia Blazizza <sblazizza@gmail.com>
2021-11-24 01:09:28 -0300

    fix: rotas em suas respectivas pastas

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require ('../models/atendimentos')
+
 module.exports = app => {
     app.get ('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos e esta realizando um GET'))
 
     app.post ('/atendimentos', (req,res) => {
-        console.log(req.body)
-        res.send ('Você está na rota de arendimentos e esta realizando um POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send ('Post atendimento')
     })
 }
