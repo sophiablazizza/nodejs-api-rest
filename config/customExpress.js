3074db06591a23e761211c783d3eb453f8851b81
Sophia Blazizza <sblazizza@gmail.com>
2021-11-23 22:26:33 -0300

    feat: dados relativos ao pagamento e objeto json (bodyparser)

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require ('express');
 const consign = require ('consign');
+const bodyParser = require ('body-parser')
 
 module.exports = () => {
     const app = express ()
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true}));
 
     consign ()
     .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,8 @@
 module.exports = app => {
     app.get ('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos e esta realizando um GET'))
 
-    app.post ('/atendimentos', (req,res) => res.send ('Você está na rota de arendimentos e esta realizando um POST'))
+    app.post ('/atendimentos', (req,res) => {
+        console.log(req.body)
+        res.send ('Você está na rota de arendimentos e esta realizando um POST')
+    })
 }
